402c43234ffb5120d7bd8793932bd542af5ae04b
EventGamer67 <islamoffdanil67@gmail.com>
2024-01-27 20:46:43 +0500

    asd

---
+++ Assets/Spinner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //При старте
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        //
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CoffeeDelivery
+ simple project on unity
